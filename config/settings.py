944b70fe04f3adace0b11b41e9d2547c2e893bf2
Saeid Saadatigero <121683582+saeidsaadatigero@users.noreply.github.com>
2025-01-01 18:48:35 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -158,7 +158,6 @@ MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
